cbc6976ed408daf33f263d4d882c3c6520e9eded
Kent K. SD-56 <baronvontactics1@gmail.com>
2025-06-22 10:21:43 -0400

    Added image domains acceptable

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export",
+  images: {
+    domains: ["test.monkemanh.com"],
+  }
 };
 
 export default nextConfig;
